00f618d83fe95e8af22a79851c95aa13759f91da
lorenzo chelini <l.chelini@icloud.com>
2022-11-17 18:09:21 +0100

    Update and rename main.cpp to main.sh

--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-  std::cout << "hello lorenzo! << std::endl;
-  return 0;
-}
---
+++ main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Hello World second push!
+echo Hello World first push!
+echo Hello World first push!
+echo Hello World first push!
